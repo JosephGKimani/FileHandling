82caf12d65bed8784c6be46add4d084c6f744904
Joseph Gitonga <josephkimanigitonga370@gmail.com>
2024-10-09 21:48:41 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System;
+using System.Text;
+class Program
+{
+
+
+    static void Main()
+    {
+
+        string path = @"D:\txt1.txt";
+      
+
+        if (File.Exists(path))
+        {
+            string[] lines = { "i want you", "why are you doing that" };
+            File.WriteAllLines(path, lines);
+        }
+
+
+
+    }
+
+
+}
+
